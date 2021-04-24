98ab2c52876f36dc2b0cf420e0bb587cbaf320be
Ash K <tetrapteryxgames@gmail.com>
2021-04-24 10:27:53 -0300

    Basic controls

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Slider slider;
+    public float maxSupply = 100;
+    public float lossRate;
+    public bool active = true;
+
+    private float supply;
+
+    void Start()
+    {
+        supply = maxSupply;
+    }
+
+    void FixedUpdate()
+    {
+        if (!active) return;
+
+        supply -= lossRate * Time.deltaTime;
+        slider.value = supply / maxSupply;
+
+        if (supply <= 0)
+        {
+            if (playerController.collectible)
+            {
+                playerController.collectible.transform.SetParent(null);
+                playerController.collectible = null;
+            }
+            Exit();
+        }
+    }
+
+    public void UpgradeSupply()
+    {
+        // TODO: get from power up?
+        var upgrade = 25;
+
+        var rectTransform = slider.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(rectTransform.rect.width + upgrade, rectTransform.rect.height);
+        maxSupply += upgrade;
+        supply = maxSupply;
+    }
+
+    public void Exit()
+    {
+        if (playerController.collectible) UpgradeSupply();
+        playerController.Reset();
+        active = false;
+    }
+
+    public void Enter()
+    {
+        SetCameraY(0);
+        active = true;
+        supply = maxSupply;
+    }
+
+    public void SetCameraY(float y)
+    {
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameController gameController;
+    public float floatSpeed;
+
+    private new Rigidbody2D rigidbody;
+    private new Camera camera;
+
+    public GameObject collectible;
+
+    private void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        if (!gameController.active) return;
+
+        var screenCenter = camera.ScreenToWorldPoint(new Vector3(
+            Screen.width * 0.5f,
+            Screen.height * 0.5f,
+            camera.nearClipPlane));
+
+        if (rigidbody.velocity.y > 0 && transform.position.y >= screenCenter.y)
+        {
+            gameController.SetCameraY(transform.position.y);
+        }
+
+        float xMotion = Input.GetAxis("Horizontal") * floatSpeed * Time.deltaTime;
+        rigidbody.velocity = new Vector3(xMotion, rigidbody.velocity.y);
+
+        if (collectible)
+        {
+            rigidbody.gravityScale = 1;
+            return;
+        }
+
+        if (Input.GetAxis("Fire1") == 1)
+        {
+            rigidbody.gravityScale = 1;
+        }
+        else
+        {
+            rigidbody.gravityScale = -1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Exit"))
+        {
+            gameController.Exit();
+        } else if (!collectible && other.CompareTag("Collectible"))
+        {
+            collectible = other.gameObject;
+            collectible.transform.SetParent(transform);
+        }
+    }
+
+    public void Reset()
+    {
+        Destroy(collectible);
+        transform.position = Vector2.zero;
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.gravityScale = 0;
+    }
+}
