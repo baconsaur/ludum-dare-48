00fccecb48a7443738ecfbfa7fa643f18ed9c8db
Ash K <tetrapteryxgames@gmail.com>
2021-04-26 16:47:02 -0300

    Add main menu

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
 public class MusicPlayer : MonoBehaviour
 {
+    private static bool musicOff;
     static bool musicBegin;
     public AudioSource music;
     void Awake()
     {
-        if (!musicBegin) {
+        if (!musicBegin && !musicOff) {
             music.Play ();
             DontDestroyOnLoad (gameObject);
             musicBegin = true;
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        if (musicOff)
+        {
+            musicOff = false;
+            music.Play();
+        }
+        else
+        {
+            musicOff = true;
+            music.Stop();
         }
     }
 }
